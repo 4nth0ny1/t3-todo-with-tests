ed971572bb38d970a557413fc31ad25bbcb7ac7f
4nth0ny1 <acatullo4@gmail.com>
2023-07-07 20:35:31 -0500

    added discord auth and removed boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+
+export default function Home() {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <main>
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1>Create T3 App</h1>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl ">
+              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            </p>
+            <AuthShowcase />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl ">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className="rounded-2xl border border-slate-900 p-4 "
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
